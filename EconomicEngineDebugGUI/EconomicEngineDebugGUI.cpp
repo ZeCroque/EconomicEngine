6df0edf0792c436f276ed866a2c00b24254f7dc6
Bladeleef <44806600+Bladeleef@users.noreply.github.com>
2020-03-24 18:05:59 +0100

    Add QCustomPlot (#2)
    * Init Gui
    
    * Feat -> Ui: Add first slot and config library
    
    * removed_clion_files
    
    Co-authored-by: Vincent Relin <vincent.relin@metapolis.fr>
    Co-authored-by: ZeCroque <zobilezeub@gmail.com>

--- EconomicEngineDebugGUI/EconomicEngineDebugGUI.cpp
+++ EconomicEngineDebugGUI/EconomicEngineDebugGUI.cpp
@@ -1,6 +1,21 @@
 #include "EconomicEngineDebugGUI.h"
+#include <iostream>
 
-EconomicEngineDebugGUI::EconomicEngineDebugGUI(QWidget *parent) : QMainWindow(parent)
+EconomicEngineDebugGUI::EconomicEngineDebugGUI(QWidget *parent)
+	: QMainWindow(parent)
 {
 	ui.setupUi(this);
+	connect(ui.btn_add, &QPushButton::pressed,this, &EconomicEngineDebugGUI::on_btn_add_clicked);
+}
+
+void EconomicEngineDebugGUI::on_btn_add_clicked()
+{
+	ui.lb_x->setText("oui");
+
+	QMessageBox msgBox;
+	msgBox.setText("The document has been modified.");
+	msgBox.setInformativeText("Do you want to save your changes?");
+	msgBox.setStandardButtons(QMessageBox::Save | QMessageBox::Discard | QMessageBox::Cancel);
+	msgBox.setDefaultButton(QMessageBox::Save);
+	int ret = msgBox.exec();
 }
--- EconomicEngineDebugGUI/EconomicEngineDebugGUI.h
+++ EconomicEngineDebugGUI/EconomicEngineDebugGUI.h
@@ -8,8 +8,17 @@ class EconomicEngineDebugGUI : public QMainWindow
 	Q_OBJECT
 
 public:
-	EconomicEngineDebugGUI(QWidget *parent = Q_NULLPTR);
+	explicit EconomicEngineDebugGUI(QWidget *parent = Q_NULLPTR);
 
+	void addPoint(double x, double y);
+	void clearData();
+	void plot();
+	
+public slots:
+	void on_btn_add_clicked();
+	
 private:
 	Ui::EconomicEngineDebugGUIClass ui;
+	QVector<double> qv_x, qv_y;
+
 };
